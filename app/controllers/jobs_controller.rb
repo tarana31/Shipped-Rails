8d6e4c7e77b35c05a5ec373adcf331a35de7024f
Dan Mulvihill <dmulvihill3@gmail.com>
2018-09-18 15:09:26 -0400

    refactoring and added bootstrap links to layout

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,13 +2,11 @@ class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   # GET /jobs
-  # GET /jobs.json
   def index
     @jobs = Job.all
   end
 
   # GET /jobs/1
-  # GET /jobs/1.json
   def show
   end
 
@@ -22,52 +20,41 @@ class JobsController < ApplicationController
   end
 
   # POST /jobs
-  # POST /jobs.json
   def create
     @job = Job.new(job_params)
 
     respond_to do |format|
       if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
-        format.json { render :show, status: :created, location: @job }
+        redirect_to @job
+
       else
-        format.html { render :new }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+        render :new 
       end
     end
   end
 
   # PATCH/PUT /jobs/1
-  # PATCH/PUT /jobs/1.json
   def update
-    respond_to do |format|
       if @job.update(job_params)
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
-        format.json { render :show, status: :ok, location: @job }
+        redirect_to @job 
       else
-        format.html { render :edit }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+        render :edit 
       end
-    end
   end
 
   # DELETE /jobs/1
-  # DELETE /jobs/1.json
   def destroy
     @job.destroy
-    respond_to do |format|
-      format.html { redirect_to jobs_url, notice: 'Job was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to jobs_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+ 
     def set_job
       @job = Job.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+ 
     def job_params
       params.require(:job).permit(:description, :cost, :containers_needed, :boat_id, :user_id)
     end
